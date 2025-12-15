25c0bc8c828812d22bb3975845fd9817ee5a507d
Dayrelis Mena Torres <dmentor@upo.es>
2025-12-15 20:49:19 +0100

    nueva clase nodo

---
+++ src/epd9/node.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package epd9;
+
+/**
+ *
+ * @author dayrelis.mena
+ */
+public class node {
+
+    private int a;
+
+    public node() {
+        a = 5;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
